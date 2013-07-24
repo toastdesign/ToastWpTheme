a332351737062185813a44452fb518e8413a7349
Toastdesign <info@toastdesign.nl>
2013-07-24 16:13:43 +0200

    Deleted roots theme
    To much nonesense in roots theme

--- 404.php
+++ 404.php
@@ -1,13 +1,12 @@
-<?php get_template_part('templates/page', 'header'); ?>
-
-<div class="alert">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
-</div>
-
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'roots'); ?></li>
-  <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
-
+<?php get_header(); ?>
+<div id="content">
+<div id="post-0" class="post error404 not-found">
+<h1 class="entry-title"><?php _e('Not Found', 'blankslate'); ?></h1>
+<div class="entry-content">
+<p><?php _e('Nothing found for the requested page. Try a search instead?', 'blankslate'); ?></p>
 <?php get_search_form(); ?>
+</div>
+</div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- tag.php
+++ tag.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 <div id="content">
+<?php the_post(); ?>
+<h1 class="page-title"><?php _e( 'Tag Archives:', 'blankslate' ) ?> <span><?php single_tag_title() ?></span></h1>
+<?php rewind_posts(); ?>
 <?php get_template_part( 'nav', 'above' ); ?>
-<?php while ( have_posts() ) : the_post() ?>
+<?php while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
 <?php endwhile; ?>
 <?php get_template_part( 'nav', 'below' ); ?>
 </div>
